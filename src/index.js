427eb634e6d3a3ee029e099e0467ad8c9963c369
alexandragul <72790705+alexandragul@users.noreply.github.com>
2021-02-15 20:56:03 +0300

    feat: implement function ckeck
    This function checks only the right number of brackets but it doesn't check the right order

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arrayOfBrackets = str.split('');
+
+  let firstBrackets = [];
+  let secondBrackets =[];
+  
+  bracketsConfig.forEach(element => {
+    firstBrackets.push(element[0]);
+    secondBrackets.push(element[1]);
+  });
+
+  let firstBracketsArray = [];
+  let checker = true;
+
+  for (let i = 0; i < arrayOfBrackets.length; i++) {
+    if (firstBrackets.includes(arrayOfBrackets[i])) {
+      firstBracketsArray.push(arrayOfBrackets[i]);
+
+    } else if (secondBrackets.includes(arrayOfBrackets[i])) {
+        if (secondBrackets.findIndex(element => element == arrayOfBrackets[i]) != 
+        firstBracketsArray.findIndex(element => element == firstBracketsArray.pop()))
+          checker = false;
+    }
+  }
+  return checker;
 }
